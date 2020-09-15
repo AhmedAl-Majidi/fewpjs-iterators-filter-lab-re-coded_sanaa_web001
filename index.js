f7a95ea491e4bffbbf9c829bfef8de34596aafcb
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-15 14:32:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function fuzzyMatch (str, firstLetter) {
 }
 
 function matchName (str, name) {
-  let x = (driver) => driver.nam.toLowerCas() === name.toLowerCase()
+  let x = (driver) => driver.name.toLowerCas() === name.toLowerCase()
   return str.filter(x);
 }
