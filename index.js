254e80e47c265e77d9dec1f4dfbdd346057af581
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-15 14:29:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function findMatching (str, name) {
     return driver.toLowerCase() === name.toLowerCase();
   });
 }
+function fuzzyMatch (str, firstLetter) {
+  let len = firstLetter.length;
+  let x = (driver) => driver.slice(0, len) === firstLetter;
+  return str.filter(x);
+}
